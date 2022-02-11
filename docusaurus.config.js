a22b915b069e45bb8f9dd91d5e943c1ec16f7fa3
boop_xyz <jm865731@gmail.com>
2022-02-11 12:58:03 -0500

    Github Pages Fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,9 @@ const config = {
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "Atlas-Foundation", // Usually your GitHub org/user name.
+  projectName: "Atlas-Foundation.github.io", // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
